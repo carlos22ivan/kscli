98a003baccbbb61c22a2e28f9310232c9429cfeb
carlos22ivan <carlos22ivan@gmail.com>
2018-05-18 15:43:48 -0500

    version 2.0.0

--- bin/kscli.js
+++ bin/kscli.js
@@ -1,154 +1,201 @@
 #!/usr/bin/env node
 
-//  dependencias
+/**
+ * dependencias
+ **/
 const program = require('commander')
 const path = require('path')
 const fs = require('fs')
 const colors = require('colors')
 const yaml = require('js-yaml');
-const {execSync} = require('child_process');
+const {exec, execSync, spawnSync} = require('child_process');
 
-//variables globales
-let directorio = path.resolve(__dirname)
-let filesToExec = []
+const kindsCommand = {
+    srv: 'service',
+    cfm: 'configmap',
+    dep: 'deployment',
+    hpa: 'horizontalpodautoscaler',
+    ing: 'ingress',
+    nsp: 'namespace'
+}
+let directorios = []
 
-//init
 program
-    .version('1.0')
-    .arguments('[params...]')
+    .version('2.0.0')
+    .arguments('[paths...]')
     .description('aqui deberia decir lo que hace :v')
-    .option('-p, --path <path>', 'set especific path, default is directory in which the currently executing script.')
-    .option('-e, --env <env>', 'set enviroment file, default is _development.yaml.')
-    .option('-s, --service', 'add Service files.')
-    .option('-c, --configMap', 'add ConfigMap files.')
-    .option('-d, --deployment', 'add Deployment files.')
-    .option('-a, --horizontalPodAutoscaler', 'add HorizontalPodAutoscaler files.')
-    .option('-i, --ingress', 'add Ingress files.')
-    .option('-n, --namespace', 'add Namespace files.')
-    .action((params) => filesToExec = params)
+    .option('-e, --env <env>', 'set enviroment filename, default is _development.yaml.')
+    .option('-k, --kind <kinds>', 'set (srv -> service), is necessary or default is all?.')
+    .option('-f, --file <files>', 'add static files.')
+    .action((params) => directorios = params)
     .parse(process.argv)
 
-/*
-    PASOS DEL SCRIPT
+/**
+ * valida que exista un directorio seleccionado
+ * valida que no sea el directorio raiz
+ **/
+if (!directorios.length) print('directory not found.', 'error')
+directorios.forEach(directorio => {
+    if (directorio.startsWith('/')) print('directory root is dennid.', 'error')
+})
 
-    1: valida los campos
-    2: determina los archivos a ejecutar
-    3: ejecuta el script
- */
+/**
+ * valida que los globs sean solo directorios
+ **/
+let auxDirectorios = []
+directorios.forEach((directorio, index, array) => {
+    if (fs.lstatSync(directorio).isDirectory())
+        auxDirectorios.push(directorio)
+})
+directorios = auxDirectorios
 
-//<<<<<<<<<< 1: valida los datos >>>>>>>>>>//
+/**
+ * valida que exista minimo un kind
+ * crea un listado de kind requested
+ * verifica que los kind sean validos
+ * asigna el nombre del kind requested
+ * verifica que exista minimo un kind requested
+ **/
+if (!program.kind) print('-k options is required.', 'error')
+let kindsRequest = program.kind.split(',')
+kindsRequest = kindsRequest.filter((kind) => {
+    let msgWarning = `warning: the kind requested: "${kind}" is not valid kind.`
+    if (!kindsCommand[kind]) print(msgWarning, 'yellow')
+    return kindsCommand[kind]
+})
+kindsRequest.forEach((item, index, array) => {
+    array[index] = kindsCommand[item]
+})
+if (!kindsRequest.length) print('kind requested not found.', 'error')
 
-//  sobrecarga el directorio si es necesario
-if (program.path) directorio = program.path;
+/**
+ * lista los files candidatos name solicitados
+ **/
+let files = (program.file) ? program.file.split(',') : []
 
-//  valida que los parametros existan y sean .yaml
-if (filesToExec.length) validarFilesToExecExist(filesToExec)
+/**
+ * sobrecarga el nombre del enviroment si es solicitado
+ * elimina los directorios que no posean un enviroment file
+ **/
+let env = (program.env) ? program.env : '_development.yaml'
+directorios = directorios.filter(directorio => {
+    return existEnviromentFile(directorio)
+})
 
-//  valida que exista un enviroment file y sea .yaml
-let env = validarEnviromentFile(program.env)
+/**
+ * Inicializa un matriz para los archivos candidatos
+ * recorre todos los directorios y agrega candidatos
+ * crea un listado de candidatos
+ **/
+var matrizCandidatos = [];
+for (let i = 0; i < kindsRequest.length + 1; ++i) matrizCandidatos[i] = [];
+directorios.forEach((directorio) => {
+    fs.readdirSync(directorio).forEach(filename => {
+        if (!filename.endsWith('.yaml')) return
+        if (!files.indexOf(filename)) matrizCandidatos[0].push(path.join(directorio, filename))
+        let filekind = obtenerKindOfYamlFile(filename, directorio)
+        if (filekind === undefined) return
+        let kindsRequestIndex = kindsRequest.indexOf(filekind.toLowerCase())
+        if (kindsRequestIndex < 0) return
+        matrizCandidatos[kindsRequestIndex + 1].push(path.join(directorio, filename))
+    })
+})
+let listaCandidatos = []
+matrizCandidatos.forEach((value, index, array) => {
+    matrizCandidatos[index].forEach(value => {
+        listaCandidatos.push(value)
+    })
+})
+recorrerListaCandidatos(listaCandidatos, 0)
+
+/**
+ * Funciones
+ **/
 
 
-//<<<<<<<<<< 2: determina los archivos a ejecutar >>>>>>>>>>//
-
-//  agrega los archivos segun las opciones ejecutadas
-agregarDinamicFiles()
-
-
-//<<<<<<<<<< 3: ejecuta el script >>>>>>>>>>//
-
-// execute()
-
-//<<<<<<<<<< FUNCIONES >>>>>>>>>>//
-
-//valida el ambiente
-function validarEnviromentFile(env) {
-    let enviromentFileName = env ? env : '_development.yaml'
-    if (!enviromentFileName.endsWith('.yaml')) close(`error: the enviroment ${env} not .yaml format`)
-    if (!existeArchivo(enviromentFileName)) close('error: enviroment file not found in folder ' + directorio)
-    return enviromentFileName
+/**
+ * recorre la lista de candidatos de forma sincrona
+ * invoca la ejecucion del commando de forma asincrona
+ **/
+async function recorrerListaCandidatos(array, index) {
+    if (array.length === index) return
+    await ejecutarCandidato(array[index], index)
+    await recorrerListaCandidatos(array, index + 1)
 }
 
-//valida los parametros
-function validarFilesToExecExist(array) {
-    array.forEach(file => {
-        if (!existeArchivo(file)) close(`error: the file ${file} not found in ${directorio}`)
-        if (!file.endsWith('.yaml')) close(`error: the file ${file} not .yaml format`)
+/**
+ * ejecuta la ejecucion del comando de forma asincrona
+ **/
+function ejecutarCandidato(value, index) {
+    return new Promise((resolve) => {
+        let commandLine = `j2 ${value} ${env} | kubectl apply -f -`
+        // let commandLine = `ping -c 1 localhost`
+        print(`kscli =^-^= [${index + 1}/${listaCandidatos.length}] => ${commandLine}`, 'magenta');
+        let commandProcess = exec(commandLine)
+        commandProcess.stdout.on('data', (data) => {
+            print(data);
+        });
+        commandProcess.stderr.on('data', (data) => {
+            print(data, 'red');
+        });
+        commandProcess.on('close', () => {
+            resolve()
+        });
     })
 }
 
-//valida que un archivo exista en el directorio
-function existeArchivo(fileName) {
+/**
+ * valida que exista un env en el directorio
+ **/
+function existEnviromentFile(directorio) {
+    if (!existFile(env, directorio)) {
+        print(`warning: enviroment file [${env}] not found in folder ${directorio}`, 'yellow')
+        return 0
+    }
+    return 1
+}
+
+/**
+ * valida que un archivo exista en un directorio
+ **/
+function existFile(fileName, directorio) {
     if (!fs.existsSync(path.join(directorio, fileName))) return false
     return true
 }
 
-//termina la aplicacion
-function close(txt) {
-    console.log(txt.red);
-    process.exit(1);
-}
-
-//agrega el listado de files al array
-function agregarDinamicFiles() {
-
-    //  lista los tipos disponibles
-    let tiposDisponibles = crearArraySegunKind()
-    //  si no hay tipos, abandona
-    if (!tiposDisponibles.length) return
-
-    //  listado de yaml candidatos en el directorio
-    let candidatos = []
-
-    //  son candidatos si terminan en .yaml y no estan agregados aun
-    fs.readdirSync(directorio).forEach(file => {
-        if (file.endsWith('.yaml') && filesToExec.indexOf(file) === -1) candidatos.push(file)
-    })
-
-    //  si no hay candidatos, abandona
-    if (!candidatos.length) return
-
-    //  visita cada candidato
-    candidatos.forEach(candidato => {
-        //  obtiene el kind del .yaml
-        let kind = obtenerKindOfYamlFile(candidato)
-
-        //  ¿no tiene la etiquta kind? NEXT
-        if (kind === undefined) return
-
-        //if el kind es un tipo disponible, lo agrega c:
-        if (tiposDisponibles.indexOf(kind.toLowerCase()) > -1) filesToExec.push(candidato)
-    })
-}
-
-//crea un array de los tipos pasados como opcion
-function crearArraySegunKind() {
-    let tipos = []
-    if (program.service) tipos.push('service')
-    if (program.configMap) tipos.push('configmap')
-    if (program.deployment) tipos.push('deployment')
-    if (program.horizontalPodAutoscaler) tipos.push('horizontalpodautoscaler')
-    if (program.ingress) tipos.push('ingress')
-    if (program.namespace) tipos.push('namespace')
-    return tipos
-}
-
-//obtiene el valor de la etiqueta kind de un .yaml
-function obtenerKindOfYamlFile(file) {
-    let yamlObject = yaml.safeLoad(fs.readFileSync(path.join(directorio, file), 'utf8'));
-    return yamlObject.kind;
-}
-
-//execute the mistery command
-function execute() {
-
-    if (!filesToExec.length) close('error: No selected file')
-
-    for (let aux = 0; aux < filesToExec.length; ++aux) {
-        let etiqueta = `${aux + 1}/${filesToExec.length}:`
-        let command = `j2 ${filesToExec[aux]} ${env} | kubectl apply -f -`
-        console.log(`${etiqueta} command: ${command}`)
-        let stdout = execSync(command);
-        console.log(`${etiqueta}: ${stdout.toString()}`)
+/**
+ * imprime un mensaje y continua
+ **/
+function print(txt, color) {
+    switch (color) {
+        case 'yellow':
+            console.log(txt.yellow);
+            break
+        case 'red':
+            console.log(txt.red);
+            break
+        case 'blue':
+            console.log(txt.blue);
+            break
+        case 'green':
+            console.log(txt.green);
+            break
+        case 'magenta':
+            console.log(txt.magenta);
+            break
+        case 'error':
+            console.log(txt.red);
+            process.exit(1);
+            break
+        default:
+            console.log(txt);
     }
-    console.log('success =^-^='.green)
+}
+
+/**
+ * lee el valor de la etiqueta kind de un archivo .yaml
+ **/
+function obtenerKindOfYamlFile(file, directory) {
+    let yamlObject = yaml.safeLoad(fs.readFileSync(path.join(directory, file), 'utf8'));
+    return yamlObject.kind;
 }
